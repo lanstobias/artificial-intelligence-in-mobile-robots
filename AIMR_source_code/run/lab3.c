2fbb6974cb13a4107068ff48fc843eef12eb1554
BoboLillqvist <bobo.lillqvist@gmail.com>
2018-09-14 12:44:39 +0200

    Continue lab3 task1

--- AIMR_source_code/run/lab3.c
+++ AIMR_source_code/run/lab3.c
@@ -5,22 +5,70 @@ float Kp_pos, Kp_th, delta_pos, delta_th;
 //Motivate in report why we use posture and not position (float vs double)
 Posture compute_distance_to_goal(float xt, float yt)
 {
+	Posture distance_to_goal_xy;
+	Posture robot_temp_posture=GetPosture();
 	
-}
-
-float convert_distance_to_angle(Posture distance_to_goal_xy)
-{
+	distance_to_goal_xy.x=xt-robot_temp_posture.x;
+	distance_to_goal_xy.y=yt-robot_temp_posture.y;
 	
+	return distance_to_goal_xy;
 }
 
 float convert_distance_to_mm(Posture distance_to_goal_xy)
 {
-	
+	return (float)sqrt((pow(distance_to_goal_xy.x, 2)+(pow(distance_to_goal_xy.y,2))));
 }
 
-Speed calculateVelocity(float Kp, float E)
+float law_of_cos(float a, float b, float c)
 {
+	return acos((pow(a, 2) + pow(b, 2) - pow(c, 2)) / (2*a*b));
+}
+
+float calculate_Eth_old(Posture distance_to_goal_xy, float Epos, float xt, float yt)
+{
+	Posture goal_posture;
+	float distance_to_robot, distance_to_goal, angle_C;
 	
+	goal_posture.x=xt;
+	goal_posture.y=yt;
+	
+	distance_to_robot=convert_distance_to_mm(GetPosture());
+	distance_to_goal=convert_distance_to_mm(goal_posture);
+	
+	angle_C = law_of_cos(distance_to_robot,Epos,distance_to_goal);
+	
+	return PI-angle_C;	
+}
+
+float calculate_Eth(Posture distance_to_goal_xy, float Epos)
+ {
+	 float alpha, beta;
+	 
+	 alpha = asin(distance_to_goal_xy.x/Epos);
+	 beta= (PI/2) - alpha;
+	 
+	 return (GetPosture().th + beta);
+ }
+ 
+Speed calculateVelocity_angle(float Kp_th, float Eth)
+{
+	Speed turn;
+	
+	turn.r = (int)((-(Kp_th) * Eth));
+	turn.l = -(turn.r);
+	
+	return turn; 
+}
+
+
+Speed calculateVelocity_distance(float Kp_pos, float Epos)
+{
+	Speed move;
+	
+	move.r = (int)(Kp_pos * Epos);
+	move.l = move.r;
+	
+	return move;
 }
 
 
@@ -30,24 +78,26 @@ void GoTo_DaC(float xt, float yt)
 	Posture distance_to_goal_xy;
 	float Eth, Epos;
 	
-	while (abs(Epos) > delta_pos)
+	do 
 	{
 		update_position();
 		distance_to_goal_xy = compute_distance_to_goal(xt, yt);
-		Eth = convert_distance_to_angle(distance_to_goal_xy);
 		Epos = convert_distance_to_mm(distance_to_goal_xy);
+		Eth = calculate_Eth(distance_to_goal_xy, Epos);
 		
 		if (abs(Eth) > delta_th)
 		{
-			velocity=calculateVelocity(Kp_th, Eth); 
+			velocity=calculateVelocity_angle(Kp_th, Eth); 
 		}
 		else
 		{
-			velocity=calculateVelocity(Kp_pos, Epos);
+			velocity=calculateVelocity_distance(Kp_pos, Epos);
 		}
-		setSpeed(velocity.l, velocity.r);
+		//printf("Velocity.l: %d velocity.r: %d\n", velocity.l, velocity.r);
+		SetSpeed(velocity.l, velocity.r);
 		Sleep(10);
-	}
+		//printf("Epos: %f delta_pos: %f\n", Epos, delta_pos);
+	}while (abs(Epos) > delta_pos);
 	Stop();
 }
 void GoTo_MEMO()
@@ -57,12 +107,17 @@ void GoTo_MEMO()
 
 void lab3()
 {
-	
+	//ClearSteps();
 	printf("Test lab3\n");
-	Kp_pos=10;
-	Kp_th = 10;
-	delta_pos=50; 
+	Kp_pos=5;
+	Kp_th = 5;
+	delta_pos=1; 
 	delta_th=0.2;
-	Goto_DaC();
+	float xt, yt;
+	
+	xt=5;
+	yt=1;
+	
+	GoTo_DaC(xt, yt);
 }
 
--- AIMR_source_code/run/lab3.h
+++ AIMR_source_code/run/lab3.h
@@ -1,10 +1,13 @@
 #ifndef LAB3H
 #define LAB3H
 
-float compute_distance_to_goal(float xt, float yt);
-void GoTo_DaC();
+Posture compute_distance_to_goal(float xt, float yt);
+void GoTo_DaC(float xt, float yt);
 void GoTo_MEMO();
 void lab3();
 Speed calculateVelocity(float Kp, float E);
+float calculate_Eth(Posture distance_to_goal_xy, float Epos);
+float calculate_Eth_old(Posture distance_to_goal_xy, float Epos, float xt, float yt);
+float law_of_cos(float a, float b, float c);
 
 #endif
